4a4cb6d940e5707c8c44c7d3ee115c444f1c3b02
tomaszpankowski <pankowski.email@gmail.com>
2022-01-20 19:34:25 +0100

    modified routing

---
+++ src/views/shop.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+import Container from "../../node_modules/react-bootstrap/Container";
+
+class Shop extends Component{
+    render(){
+        return(        
+            <Container fluid className="shop-s1 d-flex align-items-center minh-100vh">
+                
+            </Container>    
+        );
+    }
+}
+
+export default Shop;
